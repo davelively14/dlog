4e3de39895b16d2fa0c2f6d7fb18efd86ee832e6
Dave Lively <davelively14@gmail.com>
2015-10-05 23:17:50 -0400

    Added columns to User model

---
+++ db/migrate/20151006031604_add_initial_fields_to_user.rb
@@ -0,0 +1,9 @@
+class AddInitialFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :slug, :string
+  end
+end
